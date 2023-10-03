328f0d61a687355154e5576b895d39b1096cf2e8
Jesushs4 <lagt5799@gmail.com>
2023-10-03 10:26:02 +0200

    userservice

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from './user';
 import { UserInfoFavClicked } from './user-info/user-info-fav-clicked';
 import { ToastController, ToastOptions } from '@ionic/angular';
+import { UsersService } from '../users.service';
 
 
 @Component({
@@ -12,78 +13,43 @@ import { ToastController, ToastOptions } from '@ionic/angular';
 })
 export class HomePage implements OnInit{
   
-  private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  users$:Observable<User[]>=this._users.asObservable();
-  
-  users= [
-    {
-      id: 1,
-      nombre:'jesús',
-      apellidos:'Herrera Sánchez',
-      edad:19,
-      fav:true,
-    },
-    {
-      id: 2,
-      nombre:'Denisa',
-      apellidos:'Belean',
-      edad:18,
-      fav:false,
-    },
-    {
-      id: 3,
-      nombre:'Marco',
-      apellidos:'Valente',
-      edad:30,
-      fav:false,
-    },
-    {
-      id: 4,
-      nombre:'Adrián',
-      apellidos:'perejil',
-      edad:89,
-      fav:true,
-    },
-    {
-      id: 5,
-      nombre:'Elena',
-      apellidos:'Nito Feliz',
-      edad:26,
-      fav:false,
-    }
-  ]
-  
-
-  public onFavClicked(user:User, event:UserInfoFavClicked) {
-    const users = [...this._users.value];
-    var index = users.findIndex((_user)=>_user.id == user.id);
-    
-    if (index!=-1) {
-      users[index].fav = event.fav??false;
-      this._users.next([...users]);
-    }
-    const options:ToastOptions = {
-      message:`User ${event.fav?'added':'removed'} ${event.fav?'to':'from'} favourites`,
-      duration:1000,
-      position:'bottom',
-      color: event.fav?'success':'danger',
-      cssClass:'fav-icon-toast' 
-    };
-  
-    this.toast.create(options).then(toast => toast.present());
-
-  }
 
 
- 
+
+
+
+ public load:boolean = false;
   
   constructor(
-  private toast: ToastController
+  private toast: ToastController,
+  public users:UsersService,
   ) {}
 
   ngOnInit():void {
-    setTimeout(() => {
-      this._users.next(this.users);
-    }, 1000);
+    this.load = true;
+    this.users.getAll().subscribe(users=>{this.load=false});
   }
+
+  public onFavClicked(user:User, event:UserInfoFavClicked) {
+    var _user:User = {...user};
+    _user.fav = event.fav??false;
+    this.users.updateUser(_user).subscribe(
+      {next: user=>{
+      const options:ToastOptions = {
+        message:`User ${event.fav?'added':'removed'} ${event.fav?'to':'from'} favourites`,
+        duration:1000,
+        position:'bottom',
+        color: event.fav?'success':'danger',
+        cssClass:'fav-icon-toast' 
+      };
+    
+      this.toast.create(options).then(toast => toast.present());
+    },
+    error: err=>{console.log(err);}
+    })
+
+
+
+  }
+
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from './home/user';
+
+export class UserNotFoundException extends Error {
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  public users$:Observable<User[]>=this._users.asObservable();
+
+  constructor() { }
+
+  public getAll():Observable<User[]> {
+    return new Observable(observer=>{
+      var users:User[] = [
+        {id: 1,nombre:'jesús',apellidos:'Herrera Sánchez',edad:19,fav:true,},
+        {id: 2,nombre:'Denisa',apellidos:'Belean',edad:18,fav:false,},
+        {id: 3,nombre:'Marco',apellidos:'Valente',edad:30,fav:false,},
+        {id: 4,nombre:'Adrián',apellidos:'perejil',edad:89,fav:true,},
+        {id: 5,nombre:'Elena',apellidos:'Nito Feliz',edad:26,fav:false,}
+      ];
+      this._users.next(users);
+      observer.next(users);
+      observer.complete();  
+    })
+  }
+
+  public getUser(id:number):Observable<User> {
+    return new Observable(observer=>{
+      var user = this._users.value.find(user=>user.id==id);
+      if (user) {
+        observer.next(user);
+      } else {
+        observer.error(new UserNotFoundException)
+      }
+      observer.complete();
+    })
+  }
+
+  public updateUser(user:User):Observable<User> {
+    return new Observable(observer=> {
+    var _users = [...this._users.value]
+    var index = _users.findIndex(us=>user.id==us.id);
+    if (index!=-1) {
+      _users[index] = user;
+      observer.next(user);
+      this._users.next(_users);
+    } else {
+      observer.error(new UserNotFoundException);
+    }
+  })
+  }
+
+}
